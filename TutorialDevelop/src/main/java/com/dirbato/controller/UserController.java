857d617b73cf55f95f97571b5908f25d44a0392a
dir-FY25watanabetaiki <taiki.watanabe.a372@dirbato.co.jp>
2025-06-12 16:41:59 +0900

    Lesson21 chapter6

--- TutorialDevelop/src/main/java/com/dirbato/controller/UserController.java
+++ TutorialDevelop/src/main/java/com/dirbato/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.dirbato.controller;
-import java.util.Set; // 追加
+import java.util.Set;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult; // 追加
+import org.springframework.validation.annotation.Validated; // 追加
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam; // 追加
+import org.springframework.web.bind.annotation.RequestParam;
 import com.dirbato.entity.User;
 import com.dirbato.service.UserService;
 @Controller
@@ -31,14 +33,20 @@ public class UserController {
         // User登録画面に遷移
         return "user/register";
     }
+    // ----- 変更ここから ----
     /** User登録処理 */
     @PostMapping("/register")
-    public String postRegister(User user) {
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            // エラーあり
+            return getRegister(user);
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
+    // ----- 変更ここまで ----
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
     public String getUser(@PathVariable("id") Integer id, Model model) {
@@ -55,7 +63,6 @@ public class UserController {
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // ----- 追加:ここから ----
     /** User削除処理 */
     @PostMapping(path="list", params="deleteRun")
     public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
@@ -64,5 +71,4 @@ public class UserController {
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // ----- 追加:ここまで ----
 }
