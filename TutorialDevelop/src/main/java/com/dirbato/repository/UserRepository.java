7a6503f36abf6b95fcf98a322ddba1ab00f1330e
dir-FY25watanabetaiki <taiki.watanabe.a372@dirbato.co.jp>
2025-06-12 10:57:17 +0900

    Lesson21

--- TutorialDevelop/src/main/java/com/dirbato/repository/UserRepository.java
+++ TutorialDevelop/src/main/java/com/dirbato/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.dirbato.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.dirbato.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
