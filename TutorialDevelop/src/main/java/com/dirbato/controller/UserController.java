5b9f9d78702d53a7d0593ec3fdaa13351bbbac87
dir-FY25watanabetaiki <taiki.watanabe.a372@dirbato.co.jp>
2025-06-12 12:46:40 +0900

    Lesson21

--- TutorialDevelop/src/main/java/com/dirbato/controller/UserController.java
+++ TutorialDevelop/src/main/java/com/dirbato/controller/UserController.java
@@ -3,15 +3,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute; // 追加
+import org.springframework.web.bind.annotation.PostMapping; // 追加
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.dirbato.entity.User; // 追加
 import com.dirbato.service.UserService;
-
 @Controller
 @RequestMapping("user")
 public class UserController {
     @Autowired
     private UserService service;
-
     /** 一覧画面を表示 */
     @GetMapping("/list")
     public String getList(Model model) {
@@ -20,4 +21,20 @@ public class UserController {
         // user/list.htmlに画面遷移
         return "user/list";
     }
+    // ----- 追加:ここから ----
+    /** User登録画面を表示 */
+    @GetMapping("/register")
+    public String getRegister(@ModelAttribute User user) {
+        // User登録画面に遷移
+        return "user/register";
+    }
+    /** User登録処理 */
+    @PostMapping("/register")
+    public String postRegister(User user) {
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    // ----- 追加:ここまで ----
 }
