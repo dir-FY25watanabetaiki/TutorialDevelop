ea3e975adc5227d7717a4e04d40f04c95a286102
dir-FY25watanabetaiki <taiki.watanabe.a372@dirbato.co.jp>
2025-06-12 10:20:41 +0900

    Lesson21

---
+++ TutorialDevelop/src/test/java/com/dirbato/TutorialDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dirbato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
