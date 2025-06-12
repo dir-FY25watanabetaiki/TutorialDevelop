5866f0faa924af37da124fe7974781dda6566427
dir-FY25watanabetaiki <taiki.watanabe.a372@dirbato.co.jp>
2025-06-12 16:31:26 +0900

    Lesson21 chapter5

--- TutorialDevelop/src/main/java/com/dirbato/controller/UserController.java
+++ TutorialDevelop/src/main/java/com/dirbato/controller/UserController.java
@@ -1,19 +1,22 @@
 package com.dirbato.controller;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Set; // 追加
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable; // 追加
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam; // 追加
 import com.dirbato.entity.User;
 import com.dirbato.service.UserService;
 @Controller
 @RequestMapping("user")
 public class UserController {
-    @Autowired
-    private UserService service;
+    private final UserService service;
+    public UserController(UserService service) {
+        this.service = service;
+    }
     /** 一覧画面を表示 */
     @GetMapping("/list")
     public String getList(Model model) {
@@ -36,7 +39,6 @@ public class UserController {
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // ----- 追加:ここから ----
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
     public String getUser(@PathVariable("id") Integer id, Model model) {
@@ -50,6 +52,15 @@ public class UserController {
     public String postUser(User user) {
         // User登録
         service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    // ----- 追加:ここから ----
+    /** User削除処理 */
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+        // Userを一括削除
+        service.deleteUser(idck);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
